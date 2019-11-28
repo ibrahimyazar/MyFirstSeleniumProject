e8372c8cc8d48be463a12d01c6c30ac9d1b6a698
ibrahim Yazar <ibrahimyazarusa@gmail.com>
2019-11-28 16:08:15 -0500

    Newest

---
+++ src/test/java/tests/newtest.java
@@ -0,0 +1,25 @@
+package tests;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class newtest {
+    public static WebDriver getDriver(String browserType) {
+        WebDriver driver = null;
+        if ("chrome".equals(browserType.toLowerCase())) {
+            WebDriverManager.chromedriver().setup();
+            driver = new ChromeDriver();
+        } else if ("firefox".equals(browserType.toLowerCase())) {
+            WebDriverManager.firefoxdriver().setup();
+            driver = new FirefoxDriver();
+        }
+        return driver;
+    }
+
+
+
+
+
+}
